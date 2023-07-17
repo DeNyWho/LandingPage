8c1d36fc077f395753bebbb69e6196be18e8ab40
Ахунов Денис <60143913+DeNyWho@users.noreply.github.com>
2023-07-17 18:36:16 +0300

    Add pictures, rightSide, leftSide header, mainSection

---
+++ site/src/jsMain/kotlin/com/denywho/deny/components/Header.kt
@@ -0,0 +1,71 @@
+package com.denywho.deny.components
+
+import androidx.compose.runtime.Composable
+import com.denywho.deny.components.models.Section
+import com.denywho.deny.components.models.Theme
+import com.denywho.deny.styles.LogoStyle
+import com.denywho.deny.styles.NavigationItemStyle
+import com.denywho.deny.util.Constants.FONT_FAMILY
+import com.denywho.deny.util.Res
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun Header() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(80.percent)
+            .margin(topBottom = 50.px),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LeftSide()
+        RightSide()
+    }
+}
+
+@Composable
+fun LeftSide() {
+    Row {
+        Image(
+            modifier = LogoStyle.toModifier(),
+            src = Res.Image.logo,
+            desc = "Logo Image"
+        )
+    }
+}
+
+@Composable
+fun RightSide() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .borderRadius(r = 50.px)
+            .backgroundColor(Theme.LighterGray.rgb)
+            .padding(all = 20.px),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Section.values().take(6).forEach { section ->
+            Link(
+                modifier = NavigationItemStyle.toModifier()
+                    .padding(right = 30.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(18.px)
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
+                path = section.path,
+                text = section.title
+            )
+        }
+    }
+}
---
+++ site/src/jsMain/kotlin/com/denywho/deny/sections/MainSection.kt
@@ -0,0 +1,41 @@
+package com.denywho.deny.sections
+
+import androidx.compose.runtime.Composable
+import com.denywho.deny.components.Header
+import com.denywho.deny.components.models.Section
+import com.denywho.deny.util.Constants.SECTION_WIDTH
+import com.denywho.deny.util.Res
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.id
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.silk.components.graphics.Image
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun MainSection() {
+    Box(
+        modifier = Modifier
+            .id(Section.Home.id)
+            .maxWidth(SECTION_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        MainBackground()
+        Header()
+    }
+}
+
+@Composable
+fun MainBackground() {
+    Image(
+        modifier = Modifier
+            .fillMaxSize()
+            .objectFit(ObjectFit.Cover),
+        src = Res.Image.background,
+        desc = "Background Image"
+    )
+}
